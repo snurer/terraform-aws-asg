638a469f5eb88b175ce66d456852167447187b02
snurer <suhedanureroglu@gmail.com>
2022-11-04 01:58:23 +0000

    added dynamic tag

--- asg_v1.2/asg.tf
+++ asg_v1.2/asg.tf
@@ -9,6 +9,22 @@ resource "aws_autoscaling_group" "main" {
   launch_configuration      = aws_launch_configuration.main.name
   vpc_zone_identifier       = var.vpc_zone_identifier
 
+  tag {
+    key   = ""
+    value = replace(local.name, "rtype", "main_asg")
+    propagate_at_launch = true
+  }
+
+  dynamic "tag" {
+    for_each = local.common_tags
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+  }
+
+
   lifecycle {
     create_before_destroy = true
   }
@@ -21,3 +37,5 @@ resource "aws_autoscaling_group" "main" {
   }
 
 }
+
+
